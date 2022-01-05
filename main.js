79dbdf78b9c6a6f632efdd7eec9075be794e215e
fernandobouchet <fernandobouchet@gmail.com>
2022-01-05 20:33:52 -0300

    Added win and tie logic and display

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const board = document.getElementById("board");
 const boardClass = document.querySelectorAll(".board");
+const result = document.getElementById("result");
 
 const Player = (sign) => {
   const getSign = sign;
@@ -10,6 +11,10 @@ const Player = (sign) => {
 
 const gameBoard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => {
+    return board;
+  };
 
   const setSign = (sign, index) => {
     if (board[index] === "") board[index] = sign;
@@ -24,6 +29,7 @@ const gameBoard = (() => {
   };
 
   return {
+    getBoard,
     setSign,
     getSign,
     reset,
@@ -33,30 +39,78 @@ const gameBoard = (() => {
 const game = (() => {
   let player1 = Player("X");
   let player2 = Player("O");
-
-  let playerOneTurn;
+  let playerOneTurn = true;
+  let turn = 0;
+  let gameEnd = false;
 
   const changeTurn = () => {
     playerOneTurn = !playerOneTurn;
+    turn++;
   };
 
   boardClass.forEach((element) => {
     element.addEventListener(
       "click",
       () => {
-        if (playerOneTurn) {
-          gameBoard.setSign(player1.getSign, element.id);
-          element.textContent = player1.getSign;
-          changeTurn();
-        } else {
-          gameBoard.setSign(player2.getSign, element.id);
-          element.textContent = player2.getSign;
-          changeTurn();
+        if (!gameEnd) {
+          if (playerOneTurn) {
+            gameBoard.setSign(player1.getSign, element.id);
+            element.textContent = player1.getSign;
+            checkGame(parseInt(element.id), player1);
+            changeTurn();
+          } else {
+            gameBoard.setSign(player2.getSign, element.id);
+            element.textContent = player2.getSign;
+            checkGame(parseInt(element.id), player2);
+            changeTurn();
+          }
         }
       },
       { once: true }
     );
   });
 
-  return {};
+  const playTurns = () => {};
+
+  const checkGame = (index, player) => {
+    if (checkWinner(index, player) === true) {
+      result.textContent = `${player} its the winner!`;
+      gameEnd = true;
+    } else if (turn === 8) {
+      result.textContent = `Its a tie`;
+      gameEnd = true;
+    }
+  };
+
+  const checkWinner = (index, player) => {
+    const WINNING_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return WINNING_COMBINATIONS.filter((combination) =>
+      combination.includes(index)
+    ).some((possibleCombination) =>
+      possibleCombination.every(
+        (position) => gameBoard.getSign(position) === player.getSign
+      )
+    );
+  };
+
+  const endGame = () => {
+    boardClass.forEach((element) => {
+      element.removeEventListener("click", () => {});
+    });
+  };
+
+  return {
+    checkGame,
+  };
 })();
